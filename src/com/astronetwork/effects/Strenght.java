ef40940958a2ac9f447350e860158c5c3a17a8e0
TacticalQuiver <kyleedwards@Kyles-MacBook-Pro.local>
2015-05-28 19:24:38 +0100

    Added Strength

--- src/com/astronetwork/effects/Strenght.java
+++ src/com/astronetwork/effects/Strenght.java
@@ -1,6 +1,4 @@
 package com.astronetwork.effects;
- 
-import java.util.ArrayList;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -10,11 +8,9 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
- 
-public class Effects extends JavaPlugin {
-  
-	public static ArrayList<Player> cooldown = new ArrayList<Player>();
- 
+
+public class Strenght extends JavaPlugin {
+
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 		final Player player = (Player)sender;
  
@@ -24,20 +20,20 @@ public class Effects extends JavaPlugin {
 		String blue = ChatColor.BLUE.toString();
 		String red = ChatColor.RED.toString();
  
-		if ((cmd.getName().equalsIgnoreCase("speed")) || (cmd.getName().equalsIgnoreCase("spd"))) {
-			if (this.cooldown.contains(player)) {
+		if ((cmd.getName().equalsIgnoreCase("strenght")) || (cmd.getName().equalsIgnoreCase("str"))) {
+			if (Effects.cooldown.contains(player)) {
 				player.sendMessage(dblue + "[" + blue + "AstroEffects" + dblue + "] " + red + "Please wait" + ChatColor.ITALIC + " one minute" + red + " before using this command again!");
 				return true;
 			}
-			if (!sender.hasPermission("AstroEffects.speed")) {
+			if (!sender.hasPermission("AstroEffects.strenght")) {
 				sender.sendMessage(red + "I asked a fairy if you had permission, the fairy was pissed."); } else {
 					if (args.length == 0) {
-						player.sendMessage(dblue + "[" + blue + "AstroEffects" + dblue + "]" + green + " You now have a" + white + " SPEED " + green + "buff for 5 minutes!");
-						player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 1200, 1));
-						this.cooldown.add(player);
+						player.sendMessage(dblue + "[" + blue + "AstroEffects" + dblue + "]" + green + " You now have a" + white + " STRENGHT " + green + "buff for 5 minutes!");
+						player.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 1200, 1));
+						Effects.cooldown.add(player);
 						Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
         	  public void run() {
-        		  Effects.this.cooldown.remove(player);
+        		  Effects.cooldown.remove(player);
         	  }
 						}
 						, 1200L);
@@ -45,11 +41,12 @@ public class Effects extends JavaPlugin {
 					}if (args.length == 1) {
 						@SuppressWarnings("deprecation")
 						Player targetPlayer = player.getServer().getPlayer(args[0]);
-						targetPlayer.sendMessage(dblue + "[" + blue + "AstroEffects" + dblue + "]" + green + " You now have a" + white + " SPEED " + green + "buff!");
-						targetPlayer.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 1200, 1));
+						targetPlayer.sendMessage(dblue + "[" + blue + "AstroEffects" + dblue + "]" + green + " You now have a" + white + " STRENGHT " + green + "buff!");
+						targetPlayer.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 1200, 1));
 					}
 				}
 			}
 		return false;
 	}
+	
 }
