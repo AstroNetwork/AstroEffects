6ca6a030c580856af875f3723021675f15098b14
JustNiico <hadoesminecraft@gmail.com>
2015-05-29 13:00:27 +0100

    Deleted Invis and Strength classes. More simple

--- src/com/astronetwork/effects/Effects.java
+++ src/com/astronetwork/effects/Effects.java
@@ -1,8 +1,5 @@
 package com.astronetwork.effects;
  
-import java.util.ArrayList;
-
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -12,69 +9,47 @@ import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
  
 public class Effects extends JavaPlugin {
-  
-	public static ArrayList<Player> cooldown = new ArrayList<Player>();
- 
+	
+
+	
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-		final Player player = (Player)sender;
- 
-		String white = ChatColor.WHITE.toString();
-		String green = ChatColor.GREEN.toString();
-		String dblue = ChatColor.DARK_BLUE.toString();
-		String blue = ChatColor.BLUE.toString();
-		String red = ChatColor.RED.toString();
- 
-
-		if (cmd.getName().equalsIgnoreCase("speed") || cmd.getName().equalsIgnoreCase("spd")) {
-			if (cooldown.contains(player)) {
-				player.sendMessage(dblue + "[" + blue + "AstroEffects" + dblue + "] " + red + "Please wait" + ChatColor.ITALIC + " one minute" + red + " before using this command again!");
-
-		if ((cmd.getName().equalsIgnoreCase("speed")) || (cmd.getName().equalsIgnoreCase("spd"))) {
-			if (this.cooldown.contains(player)) {
-				player.sendMessage(dblue + "[" + blue + "AstroEffects" + dblue + "] " + red + "Please wait" + red + ChatColor.ITALIC + " one minute" + red + " before using this command again!");
-
-
-		if ((cmd.getName().equalsIgnoreCase("speed")) || (cmd.getName().equalsIgnoreCase("spd"))) {
-			if (this.cooldown.contains(player)) {
-				player.sendMessage(dblue + "[" + blue + "AstroEffects" + dblue + "] " + red + "Please wait" + red + ChatColor.ITALIC + " one minute" + red + " before using this command again!");
-
+		Player p = (Player) sender;
+		if (cmd.getName().equalsIgnoreCase("speed")) {
+			if (!p.hasPermission("astroeffects.speed")) {
+				p.sendMessage(ChatColor.RED + "Just_Niico says: YOU DO NOT HAVE PERMISSION!");
+			} else {
+				p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 3000, 1));
+				p.sendMessage(ChatColor.GREEN + "The great Just_Niico and Lexxus have gave you the Speed to fight!");
+				p.sendMessage(ChatColor.GREEN + "LONG LIVE JUST_NIICO AND LEXXUS!");
+				p.sendMessage(ChatColor.LIGHT_PURPLE + "We all love them really <3 Rectoph loves them the most." + ChatColor.RED + "BUT YOU LOVE MARK OR BAN");
 				return true;
-			}
-			if (!sender.hasPermission("AstroEffects.speed")) {
-				sender.sendMessage(red + "You do not have permission!"); } else {
-					if (args.length == 0) {
-
-						player.sendMessage(dblue + "[" + blue + "AstroEffects" + dblue + "]" + green + " You now have a" + white + " SPEED " + green + "buff for 5 minutes!");
-						player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 1200, 1));
-						cooldown.add(player);
-
-						player.sendMessage(dblue + "[" + blue + "AstroEffects" + dblue + "]" + green + " You now have a" + white + " SPEED 1 " + green + "buff for 5 minutes!");
-						player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 3000, 1));
-						this.cooldown.add(player);
-						player.sendMessage(dblue + "[" + blue + "AstroEffects" + dblue + "]" + green + " You now have a" + white + " SPEED 1 " + green + "buff for 5 minutes!");
-						player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 3000, 1));
-						this.cooldown.add(player);
-						Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
-        	  public void run() {
-        		  Effects.this.cooldown.remove(player);
-        	  }
-						}
-						, 1200L);
-						return true;
-					}if (args.length == 1) {
-						@SuppressWarnings("deprecation")
-						Player targetPlayer = player.getServer().getPlayer(args[0]);
-						targetPlayer.sendMessage(dblue + "[" + blue + "AstroEffects" + dblue + "]" + green + " You now have a" + white + " SPEED 1 " + green + "buff!");
-						targetPlayer.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 3000, 1));
-					}
+		}
+		}
+			if (cmd.getName().equalsIgnoreCase("strength")) {
+				if (!p.hasPermission("astroeffects.strength")) {
+					p.sendMessage(ChatColor.RED + "Just_Niico says: YOU DO NOT HAVE PERMISSION!");
+				} else {
+					p.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 3000, 1));
+					p.sendMessage(ChatColor.GREEN + "The great Just_Niico and Lexxus have gave you the Speed to fight");
+					p.sendMessage(ChatColor.GREEN + "LONG LIVE JUST_NIICO AND LEXXUS!");
+					p.sendMessage(ChatColor.LIGHT_PURPLE + "We all love them really <3 Rectoph loves them the most." + ChatColor.RED + "BUT YOU LOVE MARK OR BAN");
+					return true;
 				}
 			}
-		return false;
+			if (cmd.getName().equalsIgnoreCase("invis")) {
+				if (!p.hasPermission("astroeffects.invis")) {
+				p.sendMessage(ChatColor.RED + "Just_Niico says: YOU DO NOT HAVE PERMISSION!");	
+				} else {
+					p.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 3000, 1));
+					p.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 3000, 1));
+					p.sendMessage(ChatColor.GREEN + "The great Just_Niico and Lexxus have gave you the Speed to fight");
+					p.sendMessage(ChatColor.GREEN + "LONG LIVE JUST_NIICO AND LEXXUS!");
+					p.sendMessage(ChatColor.LIGHT_PURPLE + "We all love them really <3 Rectoph loves them the most." + ChatColor.RED + "BUT YOU LOVE MARK OR BAN");
+					return true;
+				}
+			}
+			return true;
 	}
-}
-		return true;
-}
-}
-		return true;
-	}
+		
+	
 }
