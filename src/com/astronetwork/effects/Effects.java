572c11777987821fceca325efa00eafb199fc32b
TacticalQuiver <kyleedwards@Kyles-MacBook-Pro.local>
2015-05-30 18:37:34 +0100

    Fixed some message errors and added water breathing code.

--- src/com/astronetwork/effects/Effects.java
+++ src/com/astronetwork/effects/Effects.java
@@ -22,7 +22,7 @@ public class Effects extends JavaPlugin {
 				p.sendMessage(ChatColor.RED + "Just_Niico says: YOU DO NOT HAVE PERMISSION!");
 			} else {
 				p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 3000, 1));
-				p.sendMessage(ChatColor.GREEN + "The great Just_Niico and Lexxus have gave you the Speed to fight!");
+				p.sendMessage(ChatColor.GREEN + "The great Just_Niico and Lexxus have gave you speed to fight!");
 				p.sendMessage(ChatColor.GREEN + "LONG LIVE JUST_NIICO AND LEXUSS!");
 				p.sendMessage(ChatColor.LIGHT_PURPLE + "We all love them really <3 Rectoph loves them the most." + ChatColor.RED + "BUT YOU LOVE MARK OR BAN");
 				return true;
@@ -33,7 +33,7 @@ public class Effects extends JavaPlugin {
 					p.sendMessage(ChatColor.RED + "Just_Niico says: YOU DO NOT HAVE PERMISSION!");
 				} else {
 					p.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 3000, 1));
-					p.sendMessage(ChatColor.GREEN + "The great Just_Niico and Lexxus have gave you the Strength to fight!");
+					p.sendMessage(ChatColor.GREEN + "The great Just_Niico and Lexxus have gave you strength to fight!");
 					p.sendMessage(ChatColor.GREEN + "LONG LIVE JUST_NIICO AND LEXUSS!");
 					p.sendMessage(ChatColor.LIGHT_PURPLE + "We all love them really <3 Rectoph loves them the most." + ChatColor.RED + "BUT YOU LOVE MARK OR BAN");
 					return true;
@@ -45,7 +45,7 @@ public class Effects extends JavaPlugin {
 				} else {
 					p.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 3000, 1));
 					p.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 3000, 1));
-					p.sendMessage(ChatColor.GREEN + "The great Just_Niico and Lexxus have made you Invisible!");
+					p.sendMessage(ChatColor.GREEN + "The great Just_Niico and Lexxus have made you invisible!");
 					p.sendMessage(ChatColor.GREEN + "LONG LIVE JUST_NIICO AND LEXUSS!");
 					p.sendMessage(ChatColor.LIGHT_PURPLE + "We all love them really <3 Rectoph loves them the most." + ChatColor.RED + "BUT YOU LOVE MARK OR BAN");
 					return true;
@@ -56,7 +56,7 @@ public class Effects extends JavaPlugin {
 					p.sendMessage(ChatColor.RED + "Just_Niico says: YOU DO NOT HAVE PERMISSION!");
 				}
 				p.addPotionEffect(new PotionEffect(PotionEffectType.FAST_DIGGING, 3000, 1));
-				p.sendMessage(ChatColor.GREEN + "The great Just_Niico and Lexuss gave you the Haste to dig faster than anyone!");
+				p.sendMessage(ChatColor.GREEN + "The great Just_Niico and Lexuss gave you haste to dig faster than anyone!");
 				p.sendMessage(ChatColor.GREEN + "LONG LIVE JUST_NIICO AND LEXUSS!");
 				p.sendMessage(ChatColor.LIGHT_PURPLE + "We all love them really <3 Rectoph loves them the most." + ChatColor.RED + "BUT YOU LOVE MARK OR BAN");
 				return true;
@@ -66,7 +66,7 @@ public class Effects extends JavaPlugin {
 					p.sendMessage(ChatColor.RED + "Just_Niico says: YOU DO NOT HAVE PERMISSION!");
 				}
 				p.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 3000, 1));
-				p.sendMessage(ChatColor.GREEN + "The great Just_Niico and Lexuss gave you the Regen to gain more hearts!");
+				p.sendMessage(ChatColor.GREEN + "The great Just_Niico and Lexuss gave you regen to gain more hearts!");
 				p.sendMessage(ChatColor.GREEN + "LONG LIVE JUST_NIICO AND LEXUSS!");
 				p.sendMessage(ChatColor.LIGHT_PURPLE + "We all love them really <3 Rectoph loves them the most." + ChatColor.RED + "BUT YOU LOVE MARK OR BAN");
 				return true;
@@ -76,7 +76,7 @@ public class Effects extends JavaPlugin {
 					p.sendMessage(ChatColor.RED + "Just_Niico says: YOU DO NOT HAVE PERMISSION!");
 				}
 				p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 3000, 1));
-				p.sendMessage(ChatColor.GREEN + "The great Just_Niico and Lexuss gave you the Jump boost to jump higher!");
+				p.sendMessage(ChatColor.GREEN + "The great Just_Niico and Lexuss gave you jump boost to jump higher!");
 				p.sendMessage(ChatColor.GREEN + "LONG LIVE JUST_NIICO AND LEXUSS!");
 				p.sendMessage(ChatColor.LIGHT_PURPLE + "We all love them really <3 Rectoph loves them the most." + ChatColor.RED + "BUT YOU LOVE MARK OR BAN");
 				return true;
@@ -86,7 +86,17 @@ public class Effects extends JavaPlugin {
 					p.sendMessage(ChatColor.RED + "Just_Niico says: YOU DO NOT HAVE PERMISSION!");
 				}
 				p.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 3000, 1));
-				p.sendMessage(ChatColor.GREEN + "The great Just_Niico and Lexuss gave you the night vision to see in the dark!");
+				p.sendMessage(ChatColor.GREEN + "The great Just_Niico and Lexuss gave you night vision to see in the dark!");
+				p.sendMessage(ChatColor.GREEN + "LONG LIVE JUST_NIICO AND LEXUSS!");
+				p.sendMessage(ChatColor.LIGHT_PURPLE + "We all love them really <3 Rectoph loves them the most." + ChatColor.RED + "BUT YOU LOVE MARK OR BAN");
+				return true;
+			}
+			if (cmd.getName().equalsIgnoreCase("waterbreath")) {
+				if (!p.hasPermission("astroeffects.waterbreathing")) {
+					p.sendMessage(ChatColor.RED + "Just_Niico says: YOU DO NOT HAVE PERMISSION!");
+				}
+				p.addPotionEffect(new PotionEffect(PotionEffectType.WATER_BREATHING, 3000, 1));
+				p.sendMessage(ChatColor.GREEN + "The great Just_Niico and Lexuss gave you water breathing to breath under water.");
 				p.sendMessage(ChatColor.GREEN + "LONG LIVE JUST_NIICO AND LEXUSS!");
 				p.sendMessage(ChatColor.LIGHT_PURPLE + "We all love them really <3 Rectoph loves them the most." + ChatColor.RED + "BUT YOU LOVE MARK OR BAN");
 				return true;
